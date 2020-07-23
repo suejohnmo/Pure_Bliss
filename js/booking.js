67801c9b58d90219435f09c0b8dba909a65d2e47
lthuita <lthuita@gmail.com>
2020-07-23 18:10:31 +0300

    adding the jquery function that send the notifications to the customer after subscribing on the contact-us form

--- js/booking.js
+++ js/booking.js
@@ -16,4 +16,18 @@ function div_show() {
 function div_hide() {
     document.getElementById('pb-form').style.display = "none";
 }
+//Function for Contact-us form notifications
+$(document).ready(function(){
+    $("form#message-form").submit(function(event){
+      var name = $("input#username").val();
+      var email = $("input#usermail").val();
+      var message = $("textarea#comment").val();
+      if ($("input#username").val() && $("input#usermail").val()){
+        alert (name + ", we have received your message. Thank you for reaching out to us.");
+      }
+      else {
+        alert("Please enter your name and email!");
+      }
+    });
+  });
 
